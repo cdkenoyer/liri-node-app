e72452389f2d8a512f863f8e4aba4bcca592e7b7
cdkenoyer <cdkenoyer@gmail.com>
2018-04-09 22:15:55 -0700

    update comments

--- liri.js
+++ liri.js
@@ -8,16 +8,16 @@ var Twitter = require("twitter");
 
 var keys = require("./keys.js");
 
-// put in omdb
+// started with omdb as there are examples from class
 
-// Grab the movieName which will always be the third node argument.
+// Got movieName - third node argument and used michael's solution for movie titles with spaces
 var movieName = "";
 
 for(var i = 2; i < process.argv.length; i++){
     movieName = movieName + process.argv[i] + " ";
 }
 
-// Then run a request to the OMDB API with the movie specified
+// runs request to the OMDB API with the movie specified
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
 // This line is just to help us debug against the actual URL.
@@ -28,10 +28,23 @@ request(queryUrl, function(error, response, body) {
   // If the request is successful
   if (!error && response.statusCode === 200) {
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    // Parse the body of the site and recover json info
+    // figuring out how to get multiple select listings without writing a console log for each
     var bodyParsed = JSON.parse(body);
-    console.log(bodyParsed.Year);
+    console.log("Released: " + bodyParsed.Year);
     console.log("Release Year: " + JSON.parse(body).Year);
   }
 });
+
+
+// left to do: next to make liri.js take in the "movie-this" command. thinking of using comparison of "if" and equals "==="
+
+// hope to use request success as model for next apis
+
+// var spotify = new Spotify(keys.spotify);
+
+// var client = new Twitter(keys.twitter);
+
+// need to take in multiple commands for node - was thinking the to try the "switch/case" 
+
+// probably wrong but think the log.txt should be relatively simple using appendFile
